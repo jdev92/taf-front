c58f65a173239c3dbee19164e7a6a36e39f50e2f
jdev92 <juliencavaillon6@gmail.com>
2023-12-18 11:52:52 +0100

    ajout des noms sur le calendrier

--- src/pages/calendar.jsx
+++ src/pages/calendar.jsx
@@ -4,16 +4,51 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import alllocales from "@fullcalendar/core/locales-all";
 import ModalEvent from "../components/addEvent";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
+import axios from "axios";
+
 const Event = () => {
   const [showModal, setShowModal] = useState(false);
   const [events, setEvents] = useState([]);
+  const [selectedDates, setSelectedDates] = useState([]);
   const [minDate, setMinDate] = useState(new Date().toISOString());
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/calendarEvents"
+        );
+        setEvents(response.data);
+      } catch (error) {
+        console.error("Erreur lors de la récupération des événements :", error);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  const transformedEvents = events
+    ? events.flatMap((userEvent) =>
+        userEvent.dates
+          ? userEvent.dates.map((eventDate) => ({
+              title: `${userEvent.name}: ${eventDate.title}`,
+              start: eventDate.date,
+              backgroundColor:
+                eventDate.title === "Entreprise" ? "green" : "red",
+              allDay: true,
+            }))
+          : []
+      )
+    : [];
 
   const handleDateClick = (info) => {
     setShowModal(true);
-    setMinDate(info.dateStr);
+    const selectedDate = info.dateStr;
+
+    // Ajouter la nouvelle date à la liste des dates sélectionnées
+    setSelectedDates([...selectedDates, selectedDate]);
   };
 
   const handleAddEvent = (title, start, end) => {
@@ -40,6 +75,13 @@ const Event = () => {
         {showModal && (
           <ModalEvent onAddEvent={handleAddEvent} setShowModal={setShowModal} />
         )}
+        <style>
+          {`
+            .fc-time-grid {
+              display: none; // Masquer la grille de temps
+            }
+          `}
+        </style>
         <FullCalendar
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
           initialView="dayGridMonth"
@@ -61,11 +103,13 @@ const Event = () => {
           locale={"fr"}
           firstDay={1}
           selectable={true}
-          events={events}
+          events={transformedEvents}
           dateClick={handleDateClick}
           validRange={{
             start: minDate,
           }}
+          dayMaxEvents={true}
+          displayEventTime={false}
         />
       </div>
     </div>
